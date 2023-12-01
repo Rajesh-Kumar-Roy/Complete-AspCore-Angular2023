a81f67e8fced7bce764c2964ad0414e7194db1a9
Rajesh Roy <rajeshkumarroy01998@outlook.com>
2023-12-02 00:34:50 +0600

    End of section 16 API Validation Added

--- API/Extension/SwaggerServiceExtension.cs
+++ API/Extension/SwaggerServiceExtension.cs
@@ -1,10 +1,32 @@
-﻿namespace API.Extension
+﻿using Microsoft.OpenApi.Models;
+
+namespace API.Extension
 {
     public static class SwaggerServiceExtension
     {
         public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
         {
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo{Title = "SkiNet API", Version = "v1"});
+
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWt Auth Bearer Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer",securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement { { securitySchema, new[] { "Bearer" } } };
+                c.AddSecurityRequirement(securityRequirement);
+            });
             return services;
         }
 
